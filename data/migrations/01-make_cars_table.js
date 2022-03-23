8929943ac5c87d496bda7406d4b9d549aeadfc31
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-03-23 00:01:17 -0600

    first push after internet back up mvp done

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+    tbl.string('vin', 17).unique().notNullable()
+    tbl.string('make', 100).notNullable()
+    tbl.string('model', 100).notNullable()
+    tbl.integer('mileage').notNullable()
+    tbl.string('title', 100)
+    tbl.string('transmission', 100)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+      vin: '12345678911111111',
+      make: 'soul  ',
+      model: 'kia',
+      mileage: 123000,
+      title: 'clean',
+      transmission: 'manual'
+    },
+    {
+      vin: '12345678911111112',
+      make: 'volkswagen',
+      model: 'jetta',
+      mileage: 45678,
+      title: 'salvage',
+    },
+    {
+      vin: '12345678911111113',
+      make: 'toyota',
+      model: 'camry',
+      mileage: 10101
+    }
+  ]
+  
+  exports.seed = function(knex){
+    return knex('cars')
+    .truncate().then(()=>{
+      return knex('cars').insert(cars)
+    })
+  }
